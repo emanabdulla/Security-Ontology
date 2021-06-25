8899f9c18abb33ff82d976757ca311811e8b1b79
emanabdulla <emme-666@hotmail.com>
2021-06-25 08:21:50 +0100

    update reasoner, add tests

--- test/security/security_test.clj
+++ test/security/security_test.clj
@@ -1,13 +1,28 @@
 (ns security.security-test
     (:use [clojure.test])
     (:require
-     [security.security ]
+     [security.security :as s]
      [tawny.owl :as o]
      [tawny.reasoner :as r]
      [tawny.fixture :as f]))
 
+(defn ontology-reasoner-fixture [tests]
+  (r/reasoner-factory :hermit)
+  (o/ontology-to-namespace s/SecurityVocab)
+  (binding [r/*reasoner-progress-monitor*
+            (atom
+            r/reasoner-progress-monitor-silent)]
+    (tests)))
+
+(use-fixtures :once ontology-reasoner-fixture)
+
 (use-fixtures :each (f/reasoner :hermit))
 
 (deftest reasonable
-  (is (r/consistent? security.security/SecurityVocab))
-  (is (r/coherent? security.security/SecurityVocab)))
+  (is (r/consistent? s/SecurityVocab))
+  (is (r/coherent? s/SecurityVocab)))
+
+(deftest Key
+(is (r/isubclass? s/Key s/SecretKey))
+(is (not (r/isuperclass? s/Key s/SecretKey))))
+
