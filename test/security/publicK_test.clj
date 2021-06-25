1a9e7ccb16494a66bebd721aeffa5758e6cfee70
emanabdulla <emme-666@hotmail.com>
2021-06-25 05:56:53 +0100

    Updated reasoner, add ciphertext Test

--- test/security/publicK_test.clj
+++ test/security/publicK_test.clj
@@ -1,13 +1,44 @@
-(ns security.PublicK-test
+(ns security.publicK-test
     (:use [clojure.test])
     (:require
-     [security.publicK ]
+     [security.publicK :as p]
+    
      [tawny.owl :as o]
      [tawny.reasoner :as r]
      [tawny.fixture :as f]))
 
+(defn ontology-reasoner-fixture [tests]
+  (r/reasoner-factory :hermit)
+  (o/ontology-to-namespace p/PublicKeyCryptography)
+  (binding [r/*reasoner-progress-monitor*
+            (atom
+            r/reasoner-progress-monitor-silent)]
+    (tests)))
+
+(use-fixtures :once ontology-reasoner-fixture)
+
+
+
+
 (use-fixtures :each (f/reasoner :hermit))
 
+
+
 (deftest reasonable
-  (is (r/consistent? security.publicK/PublicKeyCryptography))
-  (is (r/coherent? security.publicK/PublicKeyCryptography)))
+  (is (r/consistent? p/PublicKeyCryptography))
+  (is (r/coherent? p/PublicKeyCryptography)))
+
+
+(deftest cipherText
+
+(is (r/isubclass? p/CipherText p/AliceCipherText))
+(is (r/isuperclass? p/AliceCipherText p/CipherText))
+
+(is (not (r/isubclass? p/AliceCipherText p/CipherText)))
+(is (not (r/isuperclass? p/CipherText p/AliceCipherText)))
+)
+
+
+
+
+
