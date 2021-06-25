2c8c90799147c929e26aa83d6e3a3d73a7e97560
emanabdulla <emme-666@hotmail.com>
2021-06-25 08:07:27 +0100

    add more tests

--- test/security/publicK_test.clj
+++ test/security/publicK_test.clj
@@ -36,8 +36,47 @@
 
 (is (not (r/isubclass? p/AliceCipherText p/CipherText)))
 (is (not (r/isuperclass? p/CipherText p/AliceCipherText)))
-)
+(is (not (r/isubclass? p/PlainText p/AliceCipherText)))
+(is (not (r/isubclass? p/PlainText p/BobCipherText)))
 
+(is (r/isubclass? p/CipherText p/BobCipherText))
+(is (r/isuperclass? p/BobCipherText p/CipherText))
+
+(is (not (r/isubclass? p/BobCipherText p/CipherText)))
+(is (not (r/isuperclass? p/CipherText p/BobCipherText))))
+
+
+(deftest PlainText
+
+(is (r/isubclass? p/PlainText p/AlicePlainText))
+(is (r/isuperclass? p/AlicePlainText p/PlainText))
+
+(is (not (r/isubclass? p/AlicePlainText p/PlainText)))
+(is (not (r/isuperclass? p/PlainText p/AlicePlainText)))
+
+
+
+(is (r/isubclass? p/PlainText p/BobPlainText))
+(is (r/isuperclass? p/BobPlainText p/PlainText))
+
+(is (not (r/isubclass? p/BobPlainText p/PlainText)))
+(is (not (r/isuperclass? p/PlainText p/BobPlainText))))
+
+
+(deftest PublicKey
+(is (r/isubclass? p/PublicKey p/AlicePublicKey))
+(is (r/isubclass? p/PublicKey p/BobPublicKey)))
+
+
+(deftest PrivateKey
+(is (r/isuperclass? p/AlicePrivateKey p/PrivateKey))
+(is (r/isuperclass? p/BobPrivateKey p/PrivateKey))
+
+(is (not (r/isuperclass? p/AlicePrivateKey p/PublicKey)))
+
+
+
+)
 
 
 
